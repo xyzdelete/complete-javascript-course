80f20f15c94ec280d0f0f3abf9dec7dd55239a31
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-04 21:05:26 +0200

    Section 2: Lecture 10. Values and Variables

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+console.log(
+    "Section 2: Assignment for Lecture 10. Values and Variables starts here"
+);
+
+let country = "Latvia";
+let continent = "Eurasia";
+let population = "1 500 000";
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+console.log(
+    "Section 2: Assignment for Lecture 10. Values and Variables ends here"
+);
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,26 @@
-let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
+console.log("Section 2: Lecture 10. Values and Variables starts here");
 
+let js = "amazing";
 console.log(40 + 8 + 23 - 10);
+
+console.log("Arturs");
+console.log(23);
+
+let firstName = "Matilda";
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let Person = "jonas";
+let PI = 3.1415;
+
+let myFirstJob = "Coder";
+let myCurrentJob = "Teacher";
+
+let job1 = "programmer";
+let job2 = "teacher";
+
+console.log(myFirstJob);
+
+console.log("Section 2: Lecture 10. Values and Variables ends here");
