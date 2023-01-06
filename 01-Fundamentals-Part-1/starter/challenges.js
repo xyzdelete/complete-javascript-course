d6db827f47b1a89d2ab7099db63b43bf7f09a51a
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-06 21:36:41 +0200

    Section 2: Lecture 20. Type Conversion and Coercion

--- 01-Fundamentals-Part-1/starter/challenges.js
+++ 01-Fundamentals-Part-1/starter/challenges.js
@@ -22,28 +22,28 @@
 
 // console.log("Section 2: Lecture 16. Coding Challenge #1 ends here");
 
-console.log("Section 2: Lecture 19. Coding Challenge #2 starts here");
+// console.log("Section 2: Lecture 19. Coding Challenge #2 starts here");
 
-// Data1
-// const markMass = 78;
-// const markHeight = 1.69;
-// const johnMass = 92;
-// const johnHeight = 1.95;
+// // Data1
+// // const markMass = 78;
+// // const markHeight = 1.69;
+// // const johnMass = 92;
+// // const johnHeight = 1.95;
 
-// Data2
-const markMass = 95;
-const markHeight = 1.88;
-const johnMass = 85;
-const johnHeight = 1.76;
+// // Data2
+// const markMass = 95;
+// const markHeight = 1.88;
+// const johnMass = 85;
+// const johnHeight = 1.76;
 
-const markBMI = markMass / markHeight ** 2;
+// const markBMI = markMass / markHeight ** 2;
 
-const johnBMI = johnMass / (johnHeight * johnHeight);
+// const johnBMI = johnMass / (johnHeight * johnHeight);
 
-if (markBMI > johnBMI) {
-    console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
-} else {
-    console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}!`);
-}
+// if (markBMI > johnBMI) {
+//     console.log(`Mark's BMI ${markBMI} is higher than John's ${johnBMI}!`);
+// } else {
+//     console.log(`John's BMI ${johnBMI} is higher than Mark's ${markBMI}!`);
+// }
 
-console.log("Section 2: Lecture 19. Coding Challenge #2 ends here");
+// console.log("Section 2: Lecture 19. Coding Challenge #2 ends here");
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -174,3 +174,27 @@
 // console.log(
 //     "Section 2: Lecture 18. Taking Decisions: if / else Statements ends here"
 // );
+
+// console.log("Section 2: Lecture 20. Type Conversion and Coercion starts here");
+
+// // Type conversion
+// const inputYear = "1991";
+// console.log(Number(inputYear), inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number("Arturs"));
+// console.log(typeof NaN);
+
+// console.log(String(23), 23);
+
+// // Type coercion
+// console.log("I am " + 23 + " years old ");
+// console.log("I am " + "23" + " years old ");
+// console.log("23" - "10" - 3);
+// console.log("23" + "10" + 3);
+
+// let n = "1" + 1;
+// n = n - 1;
+// console.log(n);
+
+// console.log("Section 2: Lecture 20. Type Conversion and Coercion ends here");
