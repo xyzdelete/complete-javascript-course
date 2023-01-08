ac9115b9f68a4f63ccee45d0490d156e0c321705
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-08 13:59:42 +0200

    Section 3: Lecture 32. Activating Strict Mode

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,16 @@
+"use strict";
+
+console.log("Section 3: Lecture 32. Activating Strict Mode starts here");
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio";
+// const private = 534;
+// const if = 23;
+
+console.log("Section 3: Lecture 32. Activating Strict Mode ends here");
