b81e9bbcb2e173bae6c64ed6f4471529467fb134
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-08 14:24:14 +0200

    Section 3: Lecture 33. Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,16 +1,43 @@
 "use strict";
 
-console.log("Section 3: Lecture 32. Activating Strict Mode starts here");
+// console.log("Section 3: Lecture 32. Activating Strict Mode starts here");
 
-let hasDriversLicense = false;
-const passTest = true;
+// let hasDriversLicense = false;
+// const passTest = true;
 
-if (passTest) hasDriversLicense = true;
+// if (passTest) hasDriversLicense = true;
 
-if (hasDriversLicense) console.log("I can drive :D");
+// if (hasDriversLicense) console.log("I can drive :D");
 
-// const interface = "Audio";
-// const private = 534;
-// const if = 23;
+// // const interface = "Audio";
+// // const private = 534;
+// // const if = 23;
 
-console.log("Section 3: Lecture 32. Activating Strict Mode ends here");
+// console.log("Section 3: Lecture 32. Activating Strict Mode ends here");
+
+console.log("Section 3: Lecture 33. Functions starts here");
+
+function logger() {
+    console.log("My name is Arturs");
+}
+
+// Calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+const num = Number("23");
+
+console.log("Section 3: Lecture 33. Functions ends here");
