c6b2d43e4e3cc5be9b77cbbad82ceab026c663b3
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-08 21:30:31 +0200

    Section 3: Assignment for Lecture 39. Introduction to Arrays

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -131,50 +131,50 @@
 
 // console.log("Section 3: Lecture 37. Reviewing Functions ends here");
 
-console.log("Section 3: Lecture 39. Introduction to Arrays starts here");
+// console.log("Section 3: Lecture 39. Introduction to Arrays starts here");
 
-const friend1 = "Michael";
-const friend2 = "Steven";
-const friend3 = "Peter";
+// const friend1 = "Michael";
+// const friend2 = "Steven";
+// const friend3 = "Peter";
 
-const friends = ["Michael", "Steven", "Peter"];
-console.log(friends);
+// const friends = ["Michael", "Steven", "Peter"];
+// console.log(friends);
 
-// const years = new Array(1991, 1984, 2000, 2020);
+// // const years = new Array(1991, 1984, 2000, 2020);
 
-console.log(friends[0]);
-console.log(friends[2]);
+// console.log(friends[0]);
+// console.log(friends[2]);
 
-console.log(friends.length);
-console.log(friends[friends.length - 1]);
+// console.log(friends.length);
+// console.log(friends[friends.length - 1]);
 
-friends[2] = "Jay";
-console.log(friends);
+// friends[2] = "Jay";
+// console.log(friends);
 
-const firstName = "Arturs";
-const arturs = [firstName, "Anikins", 2037 - 1991, "programmer", friends];
+// const firstName = "Arturs";
+// const arturs = [firstName, "Anikins", 2037 - 1991, "programmer", friends];
 
-console.log(arturs);
-console.log(arturs.length);
+// console.log(arturs);
+// console.log(arturs.length);
 
-// Exercise
-const calcAge = function (birthYear) {
-    return 2037 - birthYear;
-};
+// // Exercise
+// const calcAge = function (birthYear) {
+//     return 2037 - birthYear;
+// };
 
-const years = [1990, 1967, 2002, 2010, 2018];
+// const years = [1990, 1967, 2002, 2010, 2018];
 
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
 
-const ages = [
-    calcAge(years[0]),
-    calcAge(years[1]),
-    calcAge(years[years.length - 1]),
-];
+// const ages = [
+//     calcAge(years[0]),
+//     calcAge(years[1]),
+//     calcAge(years[years.length - 1]),
+// ];
 
-console.log(ages);
+// console.log(ages);
 
-console.log("Section 3: Lecture 39. Introduction to Arrays ends here");
+// console.log("Section 3: Lecture 39. Introduction to Arrays ends here");
