9b8e2b3c370d758571226621e4f2cc0b607ae7e9
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-08 15:30:30 +0200

    Section 3: Lecture 34. Function Declarations vs. Expressions

--- 02-Fundamentals-Part-2/starter/assignments.js
+++ 02-Fundamentals-Part-2/starter/assignments.js
@@ -1,18 +1,18 @@
 "use strict";
 
-console.log("Section 3: Assignment for Lecture 33. Functions starts here");
+// console.log("Section 3: Assignment for Lecture 33. Functions starts here");
 
-function describeCountry(country, population, capitalCity) {
-    return String(
-        `${country} has ${population} million people and its capital city is ${capitalCity}`
-    );
-}
-const country1 = describeCountry("Finland", 6, "Helsinki");
-const country2 = describeCountry("Latvia", 1, "Riga");
-const country3 = describeCountry("Russia", 140, "Moscow");
+// function describeCountry(country, population, capitalCity) {
+//     return String(
+//         `${country} has ${population} million people and its capital city is ${capitalCity}`
+//     );
+// }
+// const country1 = describeCountry("Finland", 6, "Helsinki");
+// const country2 = describeCountry("Latvia", 1, "Riga");
+// const country3 = describeCountry("Russia", 140, "Moscow");
 
-console.log(country1);
-console.log(country2);
-console.log(country3);
+// console.log(country1);
+// console.log(country2);
+// console.log(country3);
 
-console.log("Section 3: Assignment for Lecture 33. Functions ends here");
+// console.log("Section 3: Assignment for Lecture 33. Functions ends here");
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -41,3 +41,27 @@
 // const num = Number("23");
 
 // console.log("Section 3: Lecture 33. Functions ends here");
+
+console.log(
+    "Section 3: Lecture 34. Function Declarations vs. Expressions starts here"
+);
+
+// Function declaration
+const age1 = calcAge1(1991);
+console.log(age1);
+
+function calcAge1(birthYear) {
+    return 2037 - birthYear;
+}
+
+// Function expression
+const calcAge2 = function (birthYear) {
+    return 2037 - birthYear;
+};
+
+const age2 = calcAge2(1991);
+console.log(age1, age2);
+
+console.log(
+    "Section 3: Lecture 34. Function Declarations vs. Expressions ends here"
+);
