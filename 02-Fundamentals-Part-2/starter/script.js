46b432d358a5d29f0a63c27bfd05769ded261512
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-08 18:18:10 +0200

    Section 3: Lecture 37. Reviewing Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -106,3 +106,27 @@
 // console.log(
 //     "Section 3: Lecture 36. Functions Calling Other Functions ends here"
 // );
+
+console.log("Section 3: Lecture 37. Reviewing Functions starts here");
+
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+};
+
+const yearsUntilRetirement = function (birthYear, firstName) {
+    const age = calcAge(birthYear);
+    const retirement = 65 - age;
+
+    if (retirement > 0) {
+        console.log(`${firstName} retires in ${retirement} years`);
+        return retirement;
+    } else {
+        console.log(`${firstName} has already retired`);
+        return -1;
+    }
+};
+
+console.log(yearsUntilRetirement(1991, "Arturs"));
+console.log(yearsUntilRetirement(1970, "Mike"));
+
+console.log("Section 3: Lecture 37. Reviewing Functions ends here");
