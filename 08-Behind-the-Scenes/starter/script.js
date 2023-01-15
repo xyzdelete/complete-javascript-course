38f82fa154e5977cac28e87f3ee47d6ec7e8c552
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-15 13:38:44 +0200

    98. Regular Functions vs. Arrow Functions

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,35 +1,47 @@
 "use strict";
 
-console.log(this);
-
-const calcAge = function (birthYear) {
-    console.log(2037 - birthYear);
-    console.log(this);
-};
-
-calcAge(1991);
-
-const calcAgeArrow = (birthYear) => {
-    console.log(2037 - birthYear);
-    console.log(this);
-};
-calcAgeArrow(1998);
+var firstName = "Matilda";
 
 const arturs = {
+    firsName: "Arturs",
     year: 1991,
     calcAge: function () {
         console.log(this);
         console.log(2037 - this.year);
+
+        // const self = this;
+        // const isMillenial = function () {
+        //     console.log(self);
+        //     console.log(self.year >= 1981 && self.year <= 1996);
+        // };
+        // isMillenial();
+
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && self.this <= 1996);
+        };
+        isMillenial();
+    },
+
+    greet: () => {
+        console.log(this), console.log(`Hey ${this.firstName}`);
     },
 };
+
+arturs.greet();
 arturs.calcAge();
 
-const matilda = {
-    year: 2017,
+const addExpr = function (a, b) {
+    console.log(arguments);
+    return a + b;
 };
 
-matilda.calcAge = arturs.calcAge;
-matilda.calcAge();
+addExpr(2, 5);
+addExpr(2, 5, 8, 12);
 
-const f = arturs.calcAge;
-f();
+var addArrow = (a, b) => {
+    console.log(arguments);
+    return a + b;
+};
+
+// addArrow(2, 5, 8);
