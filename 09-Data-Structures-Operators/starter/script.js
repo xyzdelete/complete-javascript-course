15e2156cc1469b0610715650d79c5e0d1f516ae5
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-16 21:13:41 +0200

    107. Short Circuiting (&& and ||)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,41 +54,32 @@ const restaurant = {
     },
 };
 
-// 1) Destructuring
+console.log("---- OR ----");
 
-// SPREAD, because on RIGHT side of =
-const arr = [1, 2, ...[3, 4]];
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || "Arturs");
+console.log("" || "Arturs");
+console.log(true || 0);
+console.log(undefined || null);
 
-// REST, because on LEFT side of =
-const [a, b, ...others] = [1, 2, 3, 4, 5];
-console.log(a, b, others);
+console.log(undefined || 0 || "" || "Hello" || 23 || null);
 
-const [pizza, , risotto, ...otherFood] = [
-    ...restaurant.mainMenu,
-    ...restaurant.starterMenu,
-];
+restaurant.numGuests = 23;
+const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(guests1);
 
-console.log(pizza, risotto, otherFood);
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
 
-// Objects
-const { sat, ...weekdays } = restaurant.openingHours;
-console.log(sat);
+console.log("---- AND ----");
+console.log(0 && "Arturs");
+console.log(7 && "Arturs");
 
-// 2) Functions
-const add = function (...numbers) {
-    let sum = 0;
-    for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+console.log("Hello" && 23 && null && "Arturs");
 
-    console.log(sum);
-};
+// Practical example
+if (restaurant.orderPizza) {
+    restaurant.orderPizza("mushrooms", "spinach");
+}
 
-add(2, 3);
-add(5, 3, 7, 2);
-add(7, 2, 1, 2, 2, 3);
-
-const x = [23, 5, 7];
-add(...x);
-
-restaurant.orderPizza("mushrooms", "onion", "olives", "spinach");
-
-restaurant.orderPizza("mushrooms");
+restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
