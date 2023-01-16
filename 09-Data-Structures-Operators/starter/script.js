1be5747c4cfa1747bd3e0340db07479d347326b8
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-16 22:09:22 +0200

    109. Logical Assignment Operators

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,10 +54,25 @@ const restaurant = {
     },
 };
 
-restaurant.numGuests = 0;
-const guests = restaurant.numGuests || 10;
-console.log(guests);
+const rest1 = {
+    name: "Capri",
+    numGuests: 0,
+};
 
-// Nullish: null and undefined (NOT 0 or "")
-const guestCorrect = restaurant.numGuests ?? 10;
-console.log(guestCorrect);
+const rest2 = {
+    name: "La Piazza",
+    owner: "Giovanni Russi",
+};
+
+// OR assignment operator
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+rest1.numGuests ??= 10;
+rest2.numGuests ??= 10;
+
+rest1.owner &&= "<ANONYMOUS>";
+rest2.owner &&= "<ANONYMOUS>";
+
+console.log(rest1);
+console.log(rest2);
