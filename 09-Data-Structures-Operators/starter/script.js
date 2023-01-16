c3d1707e8ac7c6593045b810d4ac196ae77fa064
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-16 21:20:26 +0200

    108. The Nullish Coalescing Operator (??)

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -54,32 +54,10 @@ const restaurant = {
     },
 };
 
-console.log("---- OR ----");
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
 
-// Use ANY data type, return ANY data type, short-circuiting
-console.log(3 || "Arturs");
-console.log("" || "Arturs");
-console.log(true || 0);
-console.log(undefined || null);
-
-console.log(undefined || 0 || "" || "Hello" || 23 || null);
-
-restaurant.numGuests = 23;
-const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
-console.log(guests1);
-
-const guests2 = restaurant.numGuests || 10;
-console.log(guests2);
-
-console.log("---- AND ----");
-console.log(0 && "Arturs");
-console.log(7 && "Arturs");
-
-console.log("Hello" && 23 && null && "Arturs");
-
-// Practical example
-if (restaurant.orderPizza) {
-    restaurant.orderPizza("mushrooms", "spinach");
-}
-
-restaurant.orderPizza && restaurant.orderPizza("mushrooms", "spinach");
+// Nullish: null and undefined (NOT 0 or "")
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
