0eb1f03a7111213204b2d83b74d0f4d0958f97b3
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-18 15:14:41 +0200

    117. Maps: Fundamentals

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,28 +1,37 @@
 "use strict";
 
+const rest = new Map();
+rest.set("Name", "Classico Italiano");
+rest.set(1, "Fireze, Italy");
+console.log(rest.set(2, "Lisbon, Portugal"));
+
 const ordersSet = new Set(["Pasta", "Pizza", "Risotto", "Pasta", "Pizza"]);
 console.log(ordersSet);
 
-console.log(new Set("Jonas"));
-
-console.log(ordersSet.size);
-
-console.log(ordersSet.has("Pizza"));
-console.log(ordersSet.has("Bread"));
-ordersSet.add("Garlic Bread");
-ordersSet.add("Garlic Bread");
-ordersSet.delete("Risotto");
-// ordersSet.clear();
-console.log(ordersSet);
-
-for (const order of ordersSet) console.log(order);
-
-// Example
-const staff = ["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"];
-const staffUnique = [...new Set(staff)];
-console.log(staffUnique);
 console.log(
-    new Set(["Waiter", "Chef", "Waiter", "Manager", "Chef", "Waiter"]).size
+    rest
+        .set("categories", ["Pasta", "Pizza", "Risotto", "Pasta", "Pizza"])
+        .set("Open", 11)
+        .set("Close", 23)
+        .set(true, "We are open :D")
+        .set(false, "We are closed :(")
 );
 
-console.log(new Set("Arturs").size);
+console.log(rest.get("Name"));
+console.log(rest.get(true));
+console.log(rest.get(1));
+
+const time = 21;
+console.log(rest.get(time > rest.get("Open") && time < rest.get("Close")));
+
+console.log(rest.has("categories"));
+rest.delete(2);
+rest.clear();
+console.log(rest.size);
+const arr = [1, 2];
+rest.set(arr, "Test");
+console.log(rest);
+
+console.log(rest.get(arr));
+rest.clear();
+console.log(rest.set(document.querySelector("h1"), "Heading"));
