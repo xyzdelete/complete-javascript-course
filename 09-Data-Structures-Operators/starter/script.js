75d91b3fe6f579fc51ebd0edf8640e31d946b4ee
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-18 03:52:13 +0200

    115. Coding Challenge #2

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,85 +1,81 @@
 "use strict";
 
-const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-
-const openingHours = {
-    [weekdays[3]]: {
-        open: 12,
-        close: 22,
-    },
-    [weekdays[4]]: {
-        open: 11,
-        close: 23,
-    },
-    [weekdays[5]]: {
-        open: 0, // Open 24 hours
-        close: 24,
-    },
-};
-
-// Data needed for a later exercise
-const flights =
-    "_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30";
-
-// Data needed for first part of the section
-const restaurant = {
-    name: "Classico Italiano",
-    location: "Via Angelo Tavanti 23, Firenze, Italy",
-    categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
-    starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
-    mainMenu: ["Pizza", "Pasta", "Risotto"],
-
-    // ES6 enhanced object literals
-    openingHours,
-
-    order(starterIndex, mainIndex) {
-        return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
-    },
-
-    orderDelivery({
-        starterIndex = 1,
-        mainIndex = 1,
-        time = "20:00",
-        address,
-    }) {
-        console.log(
-            `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
-        );
-    },
-
-    orderPasta(ing1, ing2, ing3) {
-        console.log(
-            `Here is your delecious pasta with ${ing1}, ${ing2} and ${ing3}`
-        );
-    },
-
-    orderPizza: function (main, ...otherIngredients) {
-        console.log(main);
-        console.log(otherIngredients);
+const game = {
+    team1: "Bayern Munich",
+    team2: "Borrussia Dortmund",
+    players: [
+        [
+            "Neuer",
+            "Pavard",
+            "Martinez",
+            "Alaba",
+            "Davies",
+            "Kimmich",
+            "Goretzka",
+            "Coman",
+            "Muller",
+            "Gnarby",
+            "Lewandowski",
+        ],
+        [
+            "Burki",
+            "Schulz",
+            "Hummels",
+            "Akanji",
+            "Hakimi",
+            "Weigl",
+            "Witsel",
+            "Hazard",
+            "Brandt",
+            "Sancho",
+            "Gotze",
+        ],
+    ],
+    score: "4:0",
+    scored: ["Lewandowski", "Gnarby", "Lewandowski", "Hummels"],
+    date: "Nov 9th, 2037",
+    odds: {
+        team1: 1.33,
+        x: 3.25,
+        team2: 6.5,
     },
 };
 
-// Property NAMES
-const properties = Object.keys(openingHours);
-console.log(properties);
+// 1)
 
-let openStr = `We are open on ${properties.length} days: `;
-
-for (const day of properties) {
-    openStr += `${day}, `;
+for (let i = 0; i < game.scored.length; i++) {
+    console.log(`Goal ${i + 1}: ${game.scored[i]}`);
 }
-console.log(openStr);
 
-// Properties VALUES
-const values = Object.values(openingHours);
-
-console.log(values);
-
-// Entire object
-const entries = Object.entries(openingHours);
-console.log(entries);
-
-// [key, value]
-for (const [key, { open, close }] of entries) {
-    console.log(`On ${key} we open ${open} and close at ${close}`);
+// 2)
+let sum = 0;
+console.log(Object.entries(game.odds));
+for (const [i, value] of Object.entries(game.odds)) {
+    sum += value;
+    console.log(i, value);
 }
+console.log(sum);
+const average = sum / Object.entries(game.odds).length;
+console.log(`Average: ${average}`);
+
+// 3)
+
+for (const [i, value] of Object.entries(game.odds)) {
+    game[i] ?? console.log(`Odd of draw: ${value}`);
+    game[i] && console.log(`Odd of victory ${game[i]}: ${value}`);
+}
+
+// 4)
+let scorers = {};
+console.log(game.scored);
+for (let i = 0; i < game.scored.length; i++) {
+    let goals = 1;
+    let scoredName = game.scored[i];
+    scorers[scoredName] = goals;
+    for (let j = 0; j < game.scored.length; j++) {
+        if (scoredName === game.scored[j] && i != j) {
+            scorers[scoredName] = ++goals;
+        }
+    }
+}
+console.log(scorers);
