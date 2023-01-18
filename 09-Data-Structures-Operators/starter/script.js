dd30eb987d912fc2e3554799bc7c845157df5147
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-18 18:40:13 +0200

    120. Coding Challenge #3

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,49 +1,39 @@
 "use strict";
 
-const openingHours = {
-    thu: {
-        open: 12,
-        close: 22,
-    },
-    fri: {
-        open: 11,
-        close: 23,
-    },
-    sat: {
-        open: 0, // Open 24 hours
-        close: 24,
-    },
-};
-
-const question = new Map([
-    ["question", "What is the best programming language in the world?"],
-    [1, "C++"],
-    [2, "Java"],
-    [3, "JavaScript"],
-    ["correct", 3],
-    [true, "Correct ✅"],
-    [false, "Try again!"],
+const gameEvents = new Map([
+    [17, "⚽ GOAL"],
+    [36, "🔁 Substitution"],
+    [47, "⚽ GOAL"],
+    [61, "🔁 Substitution"],
+    [64, "⚠️ Yellow card"],
+    [69, "⛔ Red card"],
+    [70, "🔁 Substitution"],
+    [72, "🔁 Substitution"],
+    [76, "⚽ GOAL"],
+    [80, "⚽ GOAL"],
+    [92, "⚠️ Yellow card"],
 ]);
-console.log(question);
 
-// Convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+//1)
+const events = [...new Set(gameEvents.values())];
+console.log(events);
 
-// Quiz app
-console.log(question.get("question"));
-for (const [key, value] of question) {
-    if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+//2)
+gameEvents.delete(64);
+console.log(gameEvents);
+
+//3)
+console.log(
+    `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+    `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+//4)
+for (const [time, event] of gameEvents) {
+    time < 45 && console.log(`[FIRST HALF] ${time}: ${event}`);
+    time > 45 && time < 90 && console.log(`[SECOND HALF] ${time}: ${event}`);
 }
-// const answer = Number(prompt("Your answer"));
-// console.log(answer);
-// console.log(question.get(3));
-
-// console.log(question.get(answer === question.get("correct") ? true : false));
-
-// Convert map to array
-console.log([...question]);
-console.log(...question.entries());
-console.log(...question.keys());
-console.log(...question.values());
