d2a039085e0f141ed0bbab52a9675522eb625a6e
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-18 15:44:09 +0200

    118. Maps: Iteration

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,37 +1,49 @@
 "use strict";
 
-const rest = new Map();
-rest.set("Name", "Classico Italiano");
-rest.set(1, "Fireze, Italy");
-console.log(rest.set(2, "Lisbon, Portugal"));
+const openingHours = {
+    thu: {
+        open: 12,
+        close: 22,
+    },
+    fri: {
+        open: 11,
+        close: 23,
+    },
+    sat: {
+        open: 0, // Open 24 hours
+        close: 24,
+    },
+};
 
-const ordersSet = new Set(["Pasta", "Pizza", "Risotto", "Pasta", "Pizza"]);
-console.log(ordersSet);
+const question = new Map([
+    ["question", "What is the best programming language in the world?"],
+    [1, "C++"],
+    [2, "Java"],
+    [3, "JavaScript"],
+    ["correct", 3],
+    [true, "Correct ✅"],
+    [false, "Try again!"],
+]);
+console.log(question);
 
-console.log(
-    rest
-        .set("categories", ["Pasta", "Pizza", "Risotto", "Pasta", "Pizza"])
-        .set("Open", 11)
-        .set("Close", 23)
-        .set(true, "We are open :D")
-        .set(false, "We are closed :(")
-);
+// Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
 
-console.log(rest.get("Name"));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// Quiz app
+console.log(question.get("question"));
+for (const [key, value] of question) {
+    if (typeof key === "number") console.log(`Answer ${key}: ${value}`);
+}
+// const answer = Number(prompt("Your answer"));
+// console.log(answer);
+// console.log(question.get(3));
 
-const time = 21;
-console.log(rest.get(time > rest.get("Open") && time < rest.get("Close")));
+// console.log(question.get(answer === question.get("correct") ? true : false));
 
-console.log(rest.has("categories"));
-rest.delete(2);
-rest.clear();
-console.log(rest.size);
-const arr = [1, 2];
-rest.set(arr, "Test");
-console.log(rest);
-
-console.log(rest.get(arr));
-rest.clear();
-console.log(rest.set(document.querySelector("h1"), "Heading"));
+// Convert map to array
+console.log([...question]);
+console.log(...question.entries());
+console.log(...question.keys());
+console.log(...question.values());
