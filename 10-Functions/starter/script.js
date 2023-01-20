c5c0639c0fee8b2912acdef4ef5ef21a84aa0470
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-21 00:39:37 +0200

    136. Immediately Invoked Function Expressions (IIFE)

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,47 +1,22 @@
 "use strict";
 
-const poll = {
-    question: "What is your favourite programming language?",
-    options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
-    // This generates [0, 0, 0, 0]. More in the next section!
-    answers: new Array(4).fill(0),
-
-    //1)
-    registerNewAnswer(fn) {
-        const input = Number(
-            prompt(`
-            What is your favourite programming  language?
-            0: JavaScript
-            1: Python
-            2: Rust
-            3: C++
-            (Write option number)
-        `)
-        );
-        input >= 0 && input < 4
-            ? (this.answers[input] += 1)
-            : alert(`answer ${input} makes no sense`);
-        const type = prompt("Display as 'string' or 'array'?");
-        fn(type);
-    },
-    displayResults(type) {
-        if (type === "string" || type === "array") {
-            type === "array" && console.log(this.answers);
-            type === "string" &&
-                console.log(`Poll results are ${[...this.answers].toString()}`);
-        }
-        const arr = type.replaceAll("[", "").replaceAll("]", "").split(",");
-        for (let i = 0; i < arr.length; i++) {
-            arr[i] = Number(arr[i]);
-        }
-        arr.length > 0 && console.log(arr, arr.toString());
-    },
+const runOnce = function () {
+    console.log("This will never run again");
 };
+runOnce();
 
-// 2)
-document
-    .querySelector(".poll")
-    .addEventListener(
-        "click",
-        poll.registerNewAnswer.bind(poll, poll.displayResults.bind(poll))
-    );
+// IIFE
+(function () {
+    console.log("This will never run again");
+    const isPrivate = 23;
+})();
+
+(() => console.log("This will never run again"))();
+
+{
+    const isPrivate = 23;
+    var notPrivate = 45;
+}
+
+// console.log(isPrivate);
+console.log(notPrivate);
