87169cb21ff27cb75406a69039c8390e9f84ceb9
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-20 18:45:55 +0200

    135. Coding Challenge #1

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,99 +1,47 @@
 "use strict";
 
-const lufthansa = {
-    airLine: "Lufthansa",
-    iataCode: "LH",
-    bookings: [],
-    book(flightNum, name) {
-        console.log(
-            `${name} booked a seat on ${this.airLine} flight ${this.iataCode}${flightNum}`
+const poll = {
+    question: "What is your favourite programming language?",
+    options: ["0: JavaScript", "1: Python", "2: Rust", "3: C++"],
+    // This generates [0, 0, 0, 0]. More in the next section!
+    answers: new Array(4).fill(0),
+
+    //1)
+    registerNewAnswer(fn) {
+        const input = Number(
+            prompt(`
+            What is your favourite programming  language?
+            0: JavaScript
+            1: Python
+            2: Rust
+            3: C++
+            (Write option number)
+        `)
         );
-        this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+        input >= 0 && input < 4
+            ? (this.answers[input] += 1)
+            : alert(`answer ${input} makes no sense`);
+        const type = prompt("Display as 'string' or 'array'?");
+        fn(type);
+    },
+    displayResults(type) {
+        if (type === "string" || type === "array") {
+            type === "array" && console.log(this.answers);
+            type === "string" &&
+                console.log(`Poll results are ${[...this.answers].toString()}`);
+        }
+        const arr = type.replaceAll("[", "").replaceAll("]", "").split(",");
+        for (let i = 0; i < arr.length; i++) {
+            arr[i] = Number(arr[i]);
+        }
+        arr.length > 0 && console.log(arr, arr.toString());
     },
 };
 
-lufthansa.book(239, "Arturs Anikins");
-lufthansa.book(635, "John Smith");
-console.log(lufthansa);
-
-const eurowings = {
-    airLine: "Eurowings",
-    iataCode: "EW",
-    bookings: [],
-};
-
-const book = lufthansa.book;
-// Does not work
-// book(23, "Sarah Williams");
-
-// Call method
-book.call(eurowings, 23, "Sarah Williams");
-console.log(eurowings);
-
-book.call(lufthansa, 239, "Mary Cooper");
-console.log(lufthansa);
-
-const swiss = {
-    airLine: "Swiss Air Lines",
-    iataCode: "LX",
-    bookings: [],
-};
-
-book.call(swiss, 583, "Mary Cooper");
-console.log(swiss);
-
-// Apply method
-const flightData = [583, "George Cooper"];
-book.apply(swiss, flightData);
-console.log(swiss);
-
-book.call(swiss, ...flightData);
-console.log(swiss);
-
-// Bind method
-// book.call(eurowings, 23, "Sarah Williams");
-
-const bookEW = book.bind(eurowings);
-const bookLH = book.bind(lufthansa);
-const bookLX = book.bind(swiss);
-
-bookEW(23, "Steven Williams");
-console.log(eurowings);
-
-const bookEW23 = book.bind(eurowings, 23);
-bookEW23("Arturs Anikins");
-bookEW23("Martha Cooper");
-console.log(eurowings);
-
-// With Event Listeners
-lufthansa.planes = 300;
-lufthansa.buyPlane = function () {
-    console.log(this);
-    this.planes++;
-    console.log(this.planes);
-};
-
+// 2)
 document
-    .querySelector(".buy")
-    .addEventListener("click", lufthansa.buyPlane.bind(lufthansa));
-
-// Partial application
-const addTax = (rate, value) => value + value * rate;
-console.log(addTax(0.1, 200));
-
-const addVAT = addTax.bind(undefined, 0.23);
-// addVAT = value => value + value * 0.23
-
-console.log(addVAT(100));
-console.log(addVAT(23));
-
-// Using function returning a function
-
-const AddTaxRate = function (rate) {
-    return function (value) {
-        return value + value * rate;
-    };
-};
-const addVAT2 = AddTaxRate(0.23);
-console.log(addVAT2(100));
-console.log(addVAT2(23));
+    .querySelector(".poll")
+    .addEventListener(
+        "click",
+        poll.registerNewAnswer.bind(poll, poll.displayResults.bind(poll))
+    );
