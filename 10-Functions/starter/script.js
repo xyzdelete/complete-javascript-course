8b0ab11176cefde9301ca3c9e4c362edc0e40541
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-21 14:24:48 +0200

    138. More Closure Examples

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,22 +1,47 @@
 "use strict";
 
-const runOnce = function () {
-    console.log("This will never run again");
+// Example 1
+let f;
+
+const g = function () {
+    const a = 23;
+    f = function () {
+        console.log(a * 2);
+    };
 };
-runOnce();
 
-// IIFE
-(function () {
-    console.log("This will never run again");
-    const isPrivate = 23;
-})();
+const h = function () {
+    const b = 777;
+    f = function () {
+        console.log(b * 2);
+    };
+};
 
-(() => console.log("This will never run again"))();
+g();
+f();
 
-{
-    const isPrivate = 23;
-    var notPrivate = 45;
-}
+// Re-assigned f function
+h();
+f();
 
-// console.log(isPrivate);
-console.log(notPrivate);
+console.dir(f);
+
+// Example 2
+
+const boardPassengers = function (n, wait) {
+    const perGroup = n / 3;
+
+    setTimeout(function () {
+        console.log(`We are now boarding all ${n} passengers`);
+        console.log(`There are 3 groups, each with ${perGroup} passengers`);
+    }, wait * 1000);
+
+    console.log(`Will start boarding in ${wait} seconds`);
+};
+
+// setTimeout(function () {
+//     console.log("Timer");
+// }, 1000);
+
+const perGroup = 1000;
+boardPassengers(100, 3);
