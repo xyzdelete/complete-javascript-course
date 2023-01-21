1aac4da2256f68d2c5c93708b0374fdc2e859bd1
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-21 14:37:10 +0200

    139. Coding Challenge #2

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,47 +1,10 @@
 "use strict";
 
-// Example 1
-let f;
+(function () {
+    const header = document.querySelector("h1");
+    header.style.color = "red";
 
-const g = function () {
-    const a = 23;
-    f = function () {
-        console.log(a * 2);
-    };
-};
-
-const h = function () {
-    const b = 777;
-    f = function () {
-        console.log(b * 2);
-    };
-};
-
-g();
-f();
-
-// Re-assigned f function
-h();
-f();
-
-console.dir(f);
-
-// Example 2
-
-const boardPassengers = function (n, wait) {
-    const perGroup = n / 3;
-
-    setTimeout(function () {
-        console.log(`We are now boarding all ${n} passengers`);
-        console.log(`There are 3 groups, each with ${perGroup} passengers`);
-    }, wait * 1000);
-
-    console.log(`Will start boarding in ${wait} seconds`);
-};
-
-// setTimeout(function () {
-//     console.log("Timer");
-// }, 1000);
-
-const perGroup = 1000;
-boardPassengers(100, 3);
+    document.querySelector("body").addEventListener("click", function () {
+        header.style.color === "red" && (header.style.color = "blue");
+    });
+})();
