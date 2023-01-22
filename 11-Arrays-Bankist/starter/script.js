200b44481fd5e14ad0586e422e22cac7c0bb6eb6
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-22 14:35:43 +0200

    147. Creating DOM Elements

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,27 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
+const displayMovements = function (movements) {
+    containerMovements.innerHTML = ``;
+
+    movements.forEach(function (mov, i) {
+        const type = mov > 0 ? "deposit" : "withdrawal";
+
+        const html = `
+        <div class="movements__row">
+          <div class="movements__type           movements__type--${type}">${
+            i + 1
+        } ${type}</div>
+          <div class="movements__value">${mov}€</div>
+        </div>
+        `;
+        containerMovements.insertAdjacentHTML("afterbegin", html);
+    });
+};
+displayMovements(account1.movements);
+
+console.log(containerMovements.innerHTML);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,21 +95,3 @@ const inputClosePin = document.querySelector(".form__input--pin");
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // /////////////////////////////////////////////////
-
-// Map
-const currencies = new Map([
-    ["USD", "United States dollar"],
-    ["EUR", "Euro"],
-    ["GBP", "Pound sterling"],
-]);
-
-currencies.forEach(function (value, key, map) {
-    console.log(`${key}: ${value}`);
-});
-
-// Set
-const currenciesUnique = new Set(["USD", "GBP", "USD", "EUR", "EUR"]);
-console.log(currenciesUnique);
-currenciesUnique.forEach(function (value, _, set) {
-    console.log(`${value}: ${value}`);
-});
