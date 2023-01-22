c152f6d1c8be8774d41524ace66c4d4ea49af080
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-22 13:29:34 +0200

    143. The new at Method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,42 +65,24 @@ const inputClosePin = document.querySelector(".form__input--pin");
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-    ["USD", "United States dollar"],
-    ["EUR", "Euro"],
-    ["GBP", "Pound sterling"],
-]);
+// const currencies = new Map([
+//     ["USD", "United States dollar"],
+//     ["EUR", "Euro"],
+//     ["GBP", "Pound sterling"],
+// ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
-let arr = ["a", "b", "c", "d", "e"];
+// /////////////////////////////////////////////////
 
-// Slice
-console.log(arr.slice(2));
-console.log(arr.slice(2, 4));
-console.log(arr.slice(-2));
-console.log(arr.slice(-1));
-console.log(arr.slice(1, -2));
-console.log(arr.slice());
-console.log([...arr]);
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
 
-// Splice
-// console.log(arr.splice(2));
-arr.splice(-1);
-arr.splice(1, 2);
-console.log(arr);
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
 
-// Reverse
-arr = ["a", "b", "c", "d", "e"];
-const arr2 = ["j", "i", "h", "g", "j"];
-console.log(arr2.reverse());
-console.log(arr2);
+console.log(arr.at(-1));
 
-// Concat
-const letters = arr.concat(arr2);
-console.log(letters);
-console.log([...arr, ...arr2]);
-
-// Join
-console.log(letters.join("-"));
+console.log("Arturs".at(0));
