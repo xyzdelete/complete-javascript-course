4320408f66f69a668380e2e91ee58f8394251aa3
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-23 12:29:11 +0200

    150. The map Method

--- 11-Arrays-Bankist/starter/challenges.js
+++ 11-Arrays-Bankist/starter/challenges.js
@@ -1,33 +1,33 @@
-"use strict";
+// "use strict";
 
-console.log("Coding Challenge #1");
+// console.log("Coding Challenge #1");
 
-function checkDogs(dogsJulia, dogsKate) {
-    const dogsJuliaCopy = dogsJulia.slice();
-    console.log(dogsJuliaCopy);
-    dogsJuliaCopy.splice(0, 1);
-    dogsJuliaCopy.splice(-2, 2);
-    console.log(dogsJuliaCopy);
+// function checkDogs(dogsJulia, dogsKate) {
+//     const dogsJuliaCopy = dogsJulia.slice();
+//     console.log(dogsJuliaCopy);
+//     dogsJuliaCopy.splice(0, 1);
+//     dogsJuliaCopy.splice(-2, 2);
+//     console.log(dogsJuliaCopy);
 
-    const dogsKateCopy = dogsKate.slice();
-    console.log(dogsKateCopy);
-    dogsKateCopy.splice(0, 1);
-    dogsKateCopy.splice(-2, 2);
-    console.log(dogsKateCopy);
+//     const dogsKateCopy = dogsKate.slice();
+//     console.log(dogsKateCopy);
+//     dogsKateCopy.splice(0, 1);
+//     dogsKateCopy.splice(-2, 2);
+//     console.log(dogsKateCopy);
 
-    const corrected = [...dogsJuliaCopy, ...dogsKateCopy];
-    console.log(corrected);
-    corrected.forEach(function (dogAge, i) {
-        dogAge >= 3
-            ? console.log(
-                  `Dog number ${i + 1} is an adult, and is ${dogAge} years old`
-              )
-            : console.log(`Dog number ${i + 1} is still a puppy`);
-    });
-}
+//     const corrected = [...dogsJuliaCopy, ...dogsKateCopy];
+//     console.log(corrected);
+//     corrected.forEach(function (dogAge, i) {
+//         dogAge >= 3
+//             ? console.log(
+//                   `Dog number ${i + 1} is an adult, and is ${dogAge} years old`
+//               )
+//             : console.log(`Dog number ${i + 1} is still a puppy`);
+//     });
+// }
 
-// Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
-checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+// // Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
 
-// Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
-checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
+// // Data 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+// checkDogs([9, 16, 6, 8, 3], [10, 5, 6, 1, 4]);
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -95,3 +95,27 @@ console.log(containerMovements.innerHTML);
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // /////////////////////////////////////////////////
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+const eurToUsd = 1.1;
+
+const movementsUSD = movements.map((mov) => mov * eurToUsd);
+
+console.log(movements);
+console.log(movementsUSD);
+
+const movementsUSDFor = [];
+for (const mov of movements) {
+    movementsUSDFor.push(mov * eurToUsd);
+}
+console.log(movementsUSDFor);
+
+const movementDescriptions = movements.map(
+    (mov, i) =>
+        `Movement ${i + 1}: You ${
+            mov > 0 ? `deposited` : `withdrew`
+        } ${Math.abs(mov)}`
+);
+
+console.log(movementDescriptions);
