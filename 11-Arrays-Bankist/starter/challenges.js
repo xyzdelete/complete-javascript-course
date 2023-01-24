afe5b2281e9650799a5eef56414b9127ec40c46c
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-24 17:59:48 +0200

    156. Coding Challenge #3

--- 11-Arrays-Bankist/starter/challenges.js
+++ 11-Arrays-Bankist/starter/challenges.js
@@ -1,10 +1,10 @@
 "use strict";
 
-// function calcAverageHumanAge(dogsAges) {
-//     return dogsAges
-//         .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
-//         .filter((age) => age >= 18)
-//         .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
-// }
-// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
-// console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
+function calcAverageHumanAge(dogsAges) {
+    return dogsAges
+        .map((age) => (age <= 2 ? 2 * age : 16 + age * 4))
+        .filter((age) => age >= 18)
+        .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+}
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+console.log(calcAverageHumanAge([16, 6, 10, 5, 6, 1, 4]));
