4af274346bc17d0990ac888b9dfd0d0e951f6c7c
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-25 21:57:47 +0200

    163. Sorting Arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,10 +61,12 @@ const inputLoanAmount = document.querySelector(".form__input--loan-amount");
 const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
-function displayMovements(movements) {
+function displayMovements(movements, sort = false) {
     containerMovements.innerHTML = ``;
 
-    movements.forEach(function (mov, i) {
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+    movs.forEach(function (mov, i) {
         const type = mov > 0 ? "deposit" : "withdrawal";
 
         const html = `
@@ -218,6 +220,13 @@ btnClose.addEventListener("click", function (e) {
     }
 });
 
+let sorted = false;
+btnSort.addEventListener("click", function (e) {
+    e.preventDefault();
+    displayMovements(currentAccount.movements, !sorted);
+    sorted = !sorted;
+});
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -232,22 +241,38 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 // /////////////////////////////////////////////////
 
-const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
-console.log(arr.flat());
-console.log(arr);
+const owners = ["Arturs", "Zach", "Adam", "Martha"];
+console.log(owners.sort());
+console.log(owners);
 
-const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
-console.log(arrDeep.flat(2));
+// Numbers
+console.log(movements);
+console.log(movements.sort());
 
-// flat
-// const overalBalance = accounts
-//     .map((acc) => acc.movements)
-//     .flat()
-//     .reduce((acc, mov) => acc + mov, 0);
-// console.log(overalBalance);
+// return < 0, A, B (keep order)
+// return > 0, B, A (switch order)
+// Ascending
+// movements.sort((a, b) => {
+//     if (a > b) {
+//         return 1;
+//     }
+//     if (a < b) {
+//         return -1;
+//     }
+// });
 
-// flatMap
-const overalBalance = accounts
-    .flatMap((acc) => acc.movements)
-    .reduce((acc, mov) => acc + mov, 0);
-console.log(overalBalance);
+movements.sort((a, b) => a - b);
+
+console.log(movements);
+
+// Descending
+// movements.sort((a, b) => {
+//     if (a > b) {
+//         return -1;
+//     }
+//     if (a < b) {
+//         return 1;
+//     }
+// });
+movements.sort((a, b) => b - a);
+console.log(movements);
