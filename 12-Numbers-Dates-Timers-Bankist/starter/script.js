581b16644aab821bf4a38aa042b0bcb0a5dc68eb
Arturs Anikins <xyzdelete@protonmail.com>
2023-01-27 17:00:34 +0200

    181. Implementing a Countdown Timer

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -198,12 +198,39 @@ const updateUI = function (acc) {
 
 ///////////////////////////////////////
 // Event handlers
-let currentAccount;
+let currentAccount, timer;
 
 // FAKE ALWAYS LOGGED IN
-currentAccount = account1;
-updateUI(currentAccount);
-containerApp.style.opacity = 100;
+// currentAccount = account1;
+// updateUI(currentAccount);
+// containerApp.style.opacity = 100;
+
+const startLogOutTimer = function () {
+    const tick = function () {
+        const min = String(Math.trunc(time / 60)).padStart(2, 0);
+        const sec = String(time % 60).padStart(2, 0);
+        // In each call, print remaining time to UI
+        labelTimer.textContent = `${min}:${sec}`;
+
+        // When 0 seconds, stop timer and log out user
+        if (time === 0) {
+            clearInterval(timer);
+            labelWelcome.textContent = `Login to get started`;
+            containerApp.style.opacity = 0;
+        }
+
+        // Decrease 1 second
+        time--;
+    };
+
+    // Set time to 5 minutes
+    let time = 30;
+
+    // Call the time every second
+    tick();
+    const timer = setInterval(tick, 1000);
+    return timer;
+};
 
 btnLogin.addEventListener("click", function (e) {
     // Prevent form from submitting
@@ -251,6 +278,10 @@ btnLogin.addEventListener("click", function (e) {
         inputLoginUsername.value = inputLoginPin.value = "";
         inputLoginPin.blur();
 
+        // Timer
+        if (timer) clearInterval(timer);
+        timer = startLogOutTimer();
+
         // Update UI
         updateUI(currentAccount);
     }
@@ -280,6 +311,10 @@ btnTransfer.addEventListener("click", function (e) {
 
         // Update UI
         updateUI(currentAccount);
+
+        // Reset Timer
+        clearInterval(timer);
+        timer = startLogOutTimer();
     }
 });
 
@@ -301,6 +336,10 @@ btnLoan.addEventListener("click", function (e) {
 
             // Update UI
             updateUI(currentAccount);
+
+            // Reset Timer
+            clearInterval(timer);
+            timer = startLogOutTimer();
         }, 2500);
     }
     inputLoanAmount.value = "";
@@ -339,31 +378,3 @@ btnSort.addEventListener("click", function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
-
-// setTimeout
-const ingredients = ["olives", "spinach"];
-const pizzaTimer = setTimeout(
-    (ing1, ing2) =>
-        console.log(`Here is your pizza with ${ing1} and ${ing2}🍕`),
-    3000,
-    ...ingredients
-);
-console.log("Waiting...");
-
-if (ingredients.includes("spinach")) clearTimeout(pizzaTimer);
-
-// setTimeout
-setInterval(function () {
-    const now = new Date();
-    console.log(
-        Intl.DateTimeFormat(navigator.locale, {
-            weekday: "long",
-            year: "numeric",
-            day: "numeric",
-            month: "long",
-            hour: "numeric",
-            minute: "numeric",
-            second: "numeric",
-        }).format(now)
-    );
-}, 1000);
