8c8e303234d85b0c501bff42a5fdb60dc30a98f4
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-01 15:54:36 +0200

    216. Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,77 +1,24 @@
 "use strict";
 
-const Person = function (firstName, birthYear) {
-    // Instance properties
-    this.firstName = firstName;
-    this.birthYear = birthYear;
-
-    // Never do this
-    // this.calcAge = function () {
-    //     console.log(2037 - this.birthYear);
-    // };
-};
-
-const arturs = new Person("Arturs", 1991);
-console.log(arturs);
-
-Person.hey = function () {
-    console.log(`Hey there!`);
-    console.log(this);
-};
-
-Person.hey();
-
-// 1. New {} is created
-// 2. function is called, this = {}
-// 3. {} linked to prototype
-// 4. function automatically return {}
-
-const matilda = new Person("Matilda", 2017);
-const jack = new Person("Jack", 1975);
-console.log(matilda, jack);
-
-console.log(arturs instanceof Person);
-
-// Prototypes
-console.log(Person.prototype);
-
-Person.prototype.calcAge = this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-};
-
-// class expression
-// const PersonCl = class {};
-
-// class declaration
-class PersonCl {
-    constructor(firstName, birthYear) {
-        this.firstName = firstName;
-        this.birthYear = birthYear;
-    }
-
-    // Instance methods
-    // Methods will be added to .prototype property
+const PersonProto = {
     calcAge() {
         console.log(2037 - this.birthYear);
-    }
+    },
 
-    greet() {
-        console.log(`Hey ${this.firstName}`);
-    }
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+};
 
-    // Static method
-    static hey() {
-        console.log("Hey there!");
-        console.log(this);
-    }
-}
+const steven = Object.create(PersonProto);
+console.log(steven);
+steven.name = "Steven";
+steven.birthYear = 2002;
+steven.calcAge();
 
-const jessica = new PersonCl("Jessica", "1996");
-console.log(jessica);
-jessica.calcAge();
+console.log(steven.__proto__ === PersonProto);
 
-console.log(jessica.__proto__ === PersonCl.prototype);
-
-jessica.greet();
-
-PersonCl.hey();
+const sarah = Object.create(PersonProto);
+sarah.init("Sarah", 1979);
+sarah.calcAge();
