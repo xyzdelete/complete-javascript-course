7a84b3fa5a2d7840d11b2f05d010773329ac7ffe
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-01 20:38:27 +0200

    221. Inheritance Between "Classes": Object.create

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,64 +1,30 @@
 "use strict";
 
-// class declaration
-class PersonCl {
-    constructor(fullName, birthYear) {
-        this.fullName = fullName;
-        this.birthYear = birthYear;
-    }
-
-    // Methods will be added to .prototype property
+const PersonProto = {
     calcAge() {
         console.log(2037 - this.birthYear);
-    }
+    },
 
-    greet() {
-        console.log(`Hey ${this.fullName}`);
-    }
+    init(firstName, birthYear) {
+        this.firstName = firstName;
+        this.birthYear = birthYear;
+    },
+};
 
-    get age() {
-        return 2037 - this.birthYear;
-    }
+const steven = Object.create(PersonProto);
 
-    set fullName(name) {
-        console.log(name);
-        if (name.includes(" ")) this._fullName = name;
-        else alert(`${name} is not a full name`);
-    }
+const StudentProto = Object.create(PersonProto);
+StudentProto.init = function (firstName, birthYear, course) {
+    PersonProto.init.call(this, firstName, birthYear);
+    this.course = course;
+};
 
-    // Set a property that already exists
-    get fullName() {
-        return this._fullName;
-    }
+StudentProto.introduce = function () {
+    console.log(`My name is ${this.fullName} and I study ${this.course}`);
+};
 
-    // Static method
-    static hey() {
-        console.log("Hey there");
-    }
-}
-
-class StudentCl extends PersonCl {
-    constructor(fullName, birthYear, course) {
-        // Always needs to happen first!
-        super(fullName, birthYear, course);
-        this.course = course;
-    }
-
-    introduce() {
-        console.log(`My name is ${this.fullName} and I study ${this.course}`);
-    }
-
-    calcAge() {
-        console.log(
-            `I'm ${
-                2037 - this.birthYear
-            } years old, but as a student I fell more like ${
-                2037 - this.birthYear + 10
-            }`
-        );
-    }
-}
-
-const martha = new StudentCl("Martha Jones", 2012, "CS");
-martha.introduce();
-martha.calcAge();
+const jay = Object.create(StudentProto);
+jay.init("Jay", 2010, "CS");
+console.dir(jay);
+jay.introduce();
+jay.calcAge();
