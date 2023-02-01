f758940c8f6fd8104fef74e59ebcde7f2e96e94d
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-01 11:54:26 +0200

    208. Constructor Functions and the new Operator

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,1 +1,26 @@
-'use strict';
+"use strict";
+
+const Person = function (firstName, birthYear) {
+    // Instance properties
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+
+    // Never do this
+    // this.calcAge = function () {
+    //     console.log(2037 - this.birthYear);
+    // };
+};
+
+const arturs = new Person("Arturs", 1991);
+console.log(arturs);
+
+// 1. New {} is created
+// 2. function is called, this = {}
+// 3. {} linked to prototype
+// 4. function automatically return {}
+
+const matilda = new Person("Matilda", 2017);
+const jack = new Person("Jack", 1975);
+console.log(matilda, jack);
+
+console.log(arturs instanceof Person);
