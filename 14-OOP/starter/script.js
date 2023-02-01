0ff6355dab3260d9f524819b45cff502a1a442a3
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-01 21:23:34 +0200

    224. Encapsulation: Private Class Fields and Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -1,32 +1,40 @@
 "use strict";
 
+// Public fields
+// Private fields
+// Public methods
+// Private methods
+// (there is also the static version)
+
 class Account {
+    // 1) Public fields (instances)
+    locale = navigator.language;
+    // 2) Private fields (instances)
+    #movements = [];
+    #pin;
+    // 3) Public methods
     constructor(owner, currency, pin) {
         this.owner = owner;
         this.currency = currency;
-        this._pin = pin;
+        this.#pin = pin;
         // Protected property
-        this._movements = [];
-        this.locale = navigator.language;
+        // this._movements = [];
+        // this.locale = navigator.language;
 
         console.log(`Thanks for opening an account, ${owner}`);
     }
 
     // Public interface
     getMovements() {
-        return this._movements;
+        return this.#movements;
     }
 
     deposit(val) {
-        this._movements.push(val);
+        this.#movements.push(val);
     }
 
     withdraw(val) {
         this.deposit(-val);
-    }
-
-    _approveLoan(val) {
-        return true;
     }
 
     requestLoan(val) {
@@ -34,6 +42,16 @@ class Account {
             this.deposit(val);
             console.log(`Loan approved`);
         }
+    }
+
+    // 4) Private methods
+    // #approveLoan(val) {
+    _approveLoan(val) {
+        return true;
+    }
+
+    static helper() {
+        console.log("Helper");
     }
 }
 
@@ -49,3 +67,8 @@ console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
+// console.log(acc1.#movements);
+
+// console.log(acc.#pin);
+// console.log(acc1.#approveLoan(100));
+Account.helper();
