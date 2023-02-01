c92be47d9b1b51bbf5d2f7577529e40cddf6f697
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-01 21:29:26 +0200

    225. Chaining Methods

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -31,10 +31,12 @@ class Account {
 
     deposit(val) {
         this.#movements.push(val);
+        return this;
     }
 
     withdraw(val) {
         this.deposit(-val);
+        return this;
     }
 
     requestLoan(val) {
@@ -42,6 +44,7 @@ class Account {
             this.deposit(val);
             console.log(`Loan approved`);
         }
+        return this;
     }
 
     // 4) Private methods
@@ -67,8 +70,12 @@ console.log(acc1.getMovements());
 
 console.log(acc1);
 console.log(acc1.pin);
-// console.log(acc1.#movements);
+Account.helper();
 
+// console.log(acc1.#movements);
 // console.log(acc.#pin);
 // console.log(acc1.#approveLoan(100));
-Account.helper();
+
+// Chaining
+acc1.deposit(300).deposit(500).withdraw(35).requestLoan(25000).withdraw(4000);
+console.log(acc1.getMovements());
