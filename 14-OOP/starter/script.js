235a953b1f2ee5b744b170104b316d1eb05b7343
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-01 15:27:14 +0200

    214. Setters and Getters

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -5,8 +5,8 @@
 
 // class declaration
 class PersonCl {
-    constructor(firstName, birthYear) {
-        this.firstName = firstName;
+    constructor(fullName, birthYear) {
+        this.fullName = fullName;
         this.birthYear = birthYear;
     }
 
@@ -16,13 +16,29 @@ class PersonCl {
     }
 
     greet() {
-        console.log(`Hey ${this.firstName}`);
+        console.log(`Hey ${this.fullName}`);
+    }
+
+    get age() {
+        return 2037 - this.birthYear;
+    }
+
+    set fullName(name) {
+        console.log(name);
+        if (name.includes(" ")) this._fullName = name;
+        else alert(`${name} is not a full name`);
+    }
+
+    // Set a property that already exists
+    get fullName() {
+        return this._fullName;
     }
 }
 
-const jessica = new PersonCl("Jessica", "1996");
+const jessica = new PersonCl("Jessica Davis", 1996);
 console.log(jessica);
 jessica.calcAge();
+console.log(jessica.age);
 
 console.log(jessica.__proto__ === PersonCl.prototype);
 
@@ -31,3 +47,23 @@ jessica.greet();
 // 1. Classes are NOT hoisted
 // 2. Classes are first-class citizens
 // 3. Classes are executed in strict mode
+
+const walter = new PersonCl("Walter White", 1965);
+
+const account = {
+    owner: "Arturs",
+    movements: [200, 530, 120, 300],
+
+    get latest() {
+        return this.movements.slice(-1).pop();
+    },
+
+    set latest(mov) {
+        this.movements.push(mov);
+    },
+};
+
+console.log(account.latest);
+
+account.latest = 50;
+console.log(account.movements);
