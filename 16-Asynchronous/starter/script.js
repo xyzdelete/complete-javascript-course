01a713ebff36a80c72bcddc9623b178d9f57b8f5
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-07 01:37:37 +0200

    250. Welcome to Callback Hell

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -5,18 +5,45 @@ const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
 
-const getCountryData = function (country) {
-    const request = new XMLHttpRequest();
-    request.open("GET", `https://restcountries.com/v2/name/${country}`);
-    request.send();
-    console.log(request.responseText);
+// const getCountryData = function (country) {
+//     const request = new XMLHttpRequest();
+//     request.open("GET", `https://restcountries.com/v2/name/${country}`);
+//     request.send();
+//     console.log(request.responseText);
 
-    request.addEventListener("load", function () {
-        const [data] = JSON.parse(this.responseText);
-        console.log(data);
+//     request.addEventListener("load", function () {
+//         const [data] = JSON.parse(this.responseText);
+//         console.log(data);
 
-        const html = `
-    <article class="country">
+//         const html = `
+//     <article class="country">
+//           <img class="country__img" src="${data.flag}" />
+//           <div class="country__data">
+//             <h3 class="country__name">${data.name}</h3>
+//             <h4 class="country__region">${data.region}</h4>
+//             <p class="country__row"><span>👫</span>${(
+//                 +data.population / 1000000
+//             ).toFixed(1)}M people</p>
+//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
+//             <p class="country__row"><span>💰</span>${
+//                 data.currencies[0].name
+//             }</p>
+//           </div>
+//         </article>
+//     `;
+
+//         countriesContainer.insertAdjacentHTML("beforeend", html);
+//         countriesContainer.style.opacity = 1;
+//     });
+// };
+
+// getCountryData("portugal");
+// getCountryData("usa");
+// getCountryData("germany");
+
+const renderCountry = function (data, className = "") {
+    const html = `
+    <article class="country ${className}">
           <img class="country__img" src="${data.flag}" />
           <div class="country__data">
             <h3 class="country__name">${data.name}</h3>
@@ -32,11 +59,54 @@ const getCountryData = function (country) {
         </article>
     `;
 
-        countriesContainer.insertAdjacentHTML("beforeend", html);
-        countriesContainer.style.opacity = 1;
+    countriesContainer.insertAdjacentHTML("beforeend", html);
+    countriesContainer.style.opacity = 1;
+};
+
+const getCountryAndNeighbour = function (country) {
+    // AJAX call country 1
+    const request = new XMLHttpRequest();
+    request.open("GET", `https://restcountries.com/v2/name/${country}`);
+    request.send();
+    console.log(request.responseText);
+
+    // Render cvountry 1
+    request.addEventListener("load", function () {
+        const [data] = JSON.parse(this.responseText);
+        console.log(data);
+        renderCountry(data);
+
+        // Get neighbour country (2)
+        const neighbour = data.borders?.[0];
+
+        if (!neighbour) return;
+
+        // AJAX call country 2
+        const request2 = new XMLHttpRequest();
+        request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
+        request2.send();
+
+        request2.addEventListener("load", function () {
+            const data2 = JSON.parse(this.responseText);
+            console.log(data2);
+
+            renderCountry(data2, "neighbour");
+        });
     });
 };
 
-getCountryData("portugal");
-getCountryData("usa");
-getCountryData("germany");
+// getCountryAndNeighbour("portugal");
+getCountryAndNeighbour("usa");
+
+setTimeout(() => {
+    console.log("1 second passed");
+    setTimeout(() => {
+        console.log("2 second passed");
+        setTimeout(() => {
+            console.log("3 second passed");
+            setTimeout(() => {
+                console.log("4 second passed");
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
