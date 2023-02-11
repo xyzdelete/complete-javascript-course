cdbb71ed6ef61dc4ef5dd8038792a830beda9f8f
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-11 15:38:57 +0200

    256. Coding Challenge #1

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -30,111 +30,6 @@ const renderError = function (msg) {
     // countriesContainer.style.opacity = 1;
 };
 
-///////////////////////////////////////
-
-// const getCountryData = function (country) {
-//     const request = new XMLHttpRequest();
-//     request.open("GET", `https://restcountries.com/v2/name/${country}`);
-//     request.send();
-//     console.log(request.responseText);
-
-//     request.addEventListener("load", function () {
-//         const [data] = JSON.parse(this.responseText);
-//         console.log(data);
-
-//         const html = `
-//     <article class="country">
-//           <img class="country__img" src="${data.flag}" />
-//           <div class="country__data">
-//             <h3 class="country__name">${data.name}</h3>
-//             <h4 class="country__region">${data.region}</h4>
-//             <p class="country__row"><span>👫</span>${(
-//                 +data.population / 1000000
-//             ).toFixed(1)}M people</p>
-//             <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-//             <p class="country__row"><span>💰</span>${
-//                 data.currencies[0].name
-//             }</p>
-//           </div>
-//         </article>
-//     `;
-
-//         countriesContainer.insertAdjacentHTML("beforeend", html);
-//         countriesContainer.style.opacity = 1;
-//     });
-// };
-
-// getCountryData("portugal");
-// getCountryData("usa");
-// getCountryData("germany");
-
-// const getCountryAndNeighbour = function (country) {
-//     // AJAX call country 1
-//     const request = new XMLHttpRequest();
-//     request.open("GET", `https://restcountries.com/v2/name/${country}`);
-//     request.send();
-//     console.log(request.responseText);
-
-//     // Render cvountry 1
-//     request.addEventListener("load", function () {
-//         const [data] = JSON.parse(this.responseText);
-//         console.log(data);
-//         renderCountry(data);
-
-//         // Get neighbour country (2)
-//         const neighbour = data.borders?.[0];
-
-//         if (!neighbour) return;
-
-//         // AJAX call country 2
-//         const request2 = new XMLHttpRequest();
-//         request2.open("GET", `https://restcountries.com/v2/alpha/${neighbour}`);
-//         request2.send();
-
-//         request2.addEventListener("load", function () {
-//             const data2 = JSON.parse(this.responseText);
-//             console.log(data2);
-
-//             renderCountry(data2, "neighbour");
-//         });
-//     });
-// };
-
-// // getCountryAndNeighbour("portugal");
-// getCountryAndNeighbour("usa");
-
-// setTimeout(() => {
-//     console.log("1 second passed");
-//     setTimeout(() => {
-//         console.log("2 second passed");
-//         setTimeout(() => {
-//             console.log("3 second passed");
-//             setTimeout(() => {
-//                 console.log("4 second passed");
-//             }, 1000);
-//         }, 1000);
-//     }, 1000);
-// }, 1000);
-
-// const request = new XMLHttpRequest();
-// request.open("GET", `https://restcountries.com/v2/name/${country}`);
-// request.send();
-
-// const request = fetch("https://restcountries.com/v2/name/portugal");
-// console.log(request);
-
-// const getCountryData = function (country) {
-//     fetch(`https://restcountries.com/v2/name/${country}`)
-//         .then(function (response) {
-//             console.log(response);
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             console.log(data);
-//             renderCountry(data[0]);
-//         });
-// };
-
 const getJSON = function (url, errorMsg = "Something went wrong") {
     return fetch(url).then((response) => {
         console.log(response);
@@ -144,51 +39,6 @@ const getJSON = function (url, errorMsg = "Something went wrong") {
         return response.json();
     });
 };
-
-// const getCountryData = function (country) {
-//     // Country 1
-//     fetch(`https://restcountries.com/v2/name/${country}`)
-//         .then((response) => {
-//             console.log(response);
-
-//             if (!response.ok)
-//                 throw new Error(`Country not found (${response.status})`);
-
-//             return response.json();
-//         })
-//         .then((data) => {
-//             renderCountry(data[0]);
-//             //const neighbour = data[0].borders?.[0];
-//             const neighbour = "df";
-
-//             if (!neighbour) return;
-
-//             // Country 2
-//             return fetch(`https://restcountries.com/v2/alpha/${neighbour}`);
-//         })
-//         .then((response) => {
-//             if (!response.ok)
-//                 throw new Error(`Country not found (${response.status})`);
-
-//             response.json();
-//         })
-//         .then((data) => {
-//             renderCountry(data, "neighbour");
-//         })
-//         .catch((err) => {
-//             console.error(`${err}`);
-//             renderError(`Something went wrong ${err.message}. Try again!`);
-//         })
-//         .finally(() => {
-//             countriesContainer.style.opacity = 1;
-//         });
-// };
-
-// btn.addEventListener("click", function () {
-//     getCountryData("portugal");
-// });
-
-//getCountryData("dadadad");
 
 const getCountryData = function (country) {
     // Country 1
@@ -221,4 +71,30 @@ btn.addEventListener("click", function () {
     getCountryData("portugal");
 });
 
-getCountryData("australia");
+function whereAmI(coords) {
+    const { lat, lng } = coords;
+    console.log(lat, lng);
+    const url = `https://geocode.xyz/${lat},${lng}?geoit=json&auth=${undefined}`;
+
+    fetch(url)
+        .then((response) => {
+            console.log(response);
+
+            if (!response.status === 200)
+                throw new Error(`Error with code 403`);
+
+            return response.json();
+        })
+        .then((json) => {
+            console.log(json);
+            console.log(`Your are in ${json.state},${json.country}`);
+            getCountryData(json.country);
+        })
+        .catch((err) => {
+            console.log(`${err}`);
+        });
+}
+
+// whereAmI({ lat: 52.508, lng: 13.381 });
+// whereAmI({ lat: 19.037, lng: 72.873 });
+whereAmI({ lat: -33.933, lng: 18.474 });
