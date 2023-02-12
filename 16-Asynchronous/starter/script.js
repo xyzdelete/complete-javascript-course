92604955ff1eba1048ea6f982c63bfdb0bf05fa6
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-12 13:03:23 +0200

    258. The Event Loop in Practice

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -1,100 +1,10 @@
 "use strict";
 
-const btn = document.querySelector(".btn-country");
-const countriesContainer = document.querySelector(".countries");
-
-const renderCountry = function (data, className = "") {
-    const html = `
-    <article class="country ${className}">
-          <img class="country__img" src="${data.flag}" />
-          <div class="country__data">
-            <h3 class="country__name">${data.name}</h3>
-            <h4 class="country__region">${data.region}</h4>
-            <p class="country__row"><span>👫</span>${(
-                +data.population / 1000000
-            ).toFixed(1)}M people</p>
-            <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-            <p class="country__row"><span>💰</span>${
-                data.currencies[0].name
-            }</p>
-          </div>
-        </article>
-    `;
-
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    // countriesContainer.style.opacity = 1;
-};
-
-const renderError = function (msg) {
-    countriesContainer.insertAdjacentText("beforeend", msg);
-    // countriesContainer.style.opacity = 1;
-};
-
-const getJSON = function (url, errorMsg = "Something went wrong") {
-    return fetch(url).then((response) => {
-        console.log(response);
-
-        if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
-
-        return response.json();
-    });
-};
-
-const getCountryData = function (country) {
-    // Country 1
-    getJSON(`https://restcountries.com/v2/name/${country}`, `Country not found`)
-        .then((data) => {
-            renderCountry(data[0]);
-            const neighbour = data[0].borders?.[0];
-
-            if (!neighbour) throw new Error("No neighbour found!");
-
-            // Country 2
-            return getJSON(
-                `https://restcountries.com/v2/alpha/${neighbour}`,
-                "Country not found"
-            );
-        })
-        .then((data) => {
-            renderCountry(data, "neighbour");
-        })
-        .catch((err) => {
-            console.error(`${err}`);
-            renderError(`Something went wrong ${err.message}. Try again!`);
-        })
-        .finally(() => {
-            countriesContainer.style.opacity = 1;
-        });
-};
-
-btn.addEventListener("click", function () {
-    getCountryData("portugal");
+console.log(`Test start`);
+setTimeout(() => console.log(`0 sec timer`), 0);
+Promise.resolve(`Resolved promise 1`).then((res) => console.log(res));
+Promise.resolve(`Resolved promise 2`).then((res) => {
+    for (let i = 0; i < 10000000000; i++) {}
+    console.log(res);
 });
-
-function whereAmI(coords) {
-    const { lat, lng } = coords;
-    console.log(lat, lng);
-    const url = `https://geocode.xyz/${lat},${lng}?geoit=json&auth=${undefined}`;
-
-    fetch(url)
-        .then((response) => {
-            console.log(response);
-
-            if (!response.status === 200)
-                throw new Error(`Error with code 403`);
-
-            return response.json();
-        })
-        .then((json) => {
-            console.log(json);
-            console.log(`Your are in ${json.state},${json.country}`);
-            getCountryData(json.country);
-        })
-        .catch((err) => {
-            console.log(`${err}`);
-        });
-}
-
-// whereAmI({ lat: 52.508, lng: 13.381 });
-// whereAmI({ lat: 19.037, lng: 72.873 });
-whereAmI({ lat: -33.933, lng: 18.474 });
+console.log(`Test end`);
