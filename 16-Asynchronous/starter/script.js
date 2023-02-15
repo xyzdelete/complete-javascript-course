5565edf35d32cc67672df9e99cfd5bebdbd72454
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-15 19:37:36 +0200

    265. Running Promises in Parallel

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -64,21 +64,36 @@ const whereAmI = async function () {
     }
 };
 
-console.log(`1: Will get location`);
-// const city = whereAmI();
-// console.log(city);
+const getJSON = function (url, errorMsg = "Something went wrong") {
+    return fetch(url).then((response) => {
+        if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
 
-// whereAmI()
-//     .then((city) => console.log(`2: ${city}`))
-//     .catch((err) => console.error(`2: ${err.message}`))
-//     .finally(() => console.log(`3: Finished getting location`));
+        return response.json();
+    });
+};
 
-(async function () {
+const get3Countries = async function (c1, c2, c3) {
     try {
-        const city = await whereAmI();
-        console.log(`2: ${city}`);
+        // const [data1] = await getJSON(
+        //     `https://restcountries.com/v2/name/${c1}`
+        // );
+        // const [data2] = await getJSON(
+        //     `https://restcountries.com/v2/name/${c2}`
+        // );
+        // const [data3] = await getJSON(
+        //     `https://restcountries.com/v2/name/${c3}`
+        // );
+
+        const data = await Promise.all([
+            getJSON(`https://restcountries.com/v2/name/${c1}`),
+            getJSON(`https://restcountries.com/v2/name/${c2}`),
+            getJSON(`https://restcountries.com/v2/name/${c3}`),
+        ]);
+
+        console.log(data.map((d) => d[0].capital));
     } catch (err) {
-        console.log(`3: Finished getting location`);
+        console.error(err);
     }
-    console.log(`3: Finished getting location`);
-})();
+};
+
+get3Countries("portugal", "canada", "tanzania");
