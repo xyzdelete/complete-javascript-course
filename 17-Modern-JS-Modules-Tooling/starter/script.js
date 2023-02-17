a530b2820045ab7434c10e811a67d8e751a2c9c3
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-17 16:52:15 +0200

    277. Introduction to NPM

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,64 +1,17 @@
-// console.log("Start fetching");
-// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-// const data = await res.json();
-// console.log(data);
-// console.log("Something");
+import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
 
-// import * as ShoppingCart from "./shoppingCart.js";
+const state = {
+    cart: [
+        { product: "bread", quantity: 5 },
+        { product: "pizza", quantity: 5 },
+    ],
+    user: { loggedIn: true },
+};
 
-// const getLastPost = async function () {
-//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-//     const data = await res.json();
-//     console.log(data);
-//     return { title: data.at(-1).title, text: data.at(-1).body };
-// };
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
 
-// const lastPost = getLastPost();
-// console.log(lastPost);
+console.log(stateClone);
+state.user.loggedIn = false;
 
-// // Not very clean
-// // lastPost.then((last) => console.log(last));
-// const lastPost2 = await getLastPost();
-// console.log(lastPost2);
-
-// const ShoppingCart2 = (function () {
-//     const cart = [];
-//     const shippingCost = 10;
-//     const totalPrice = 237;
-//     const totalQuantity = 23;
-
-//     const addToCart = function (product, quantity) {
-//         cart.push({ product, quantity });
-//         console.log(
-//             `${quantity} ${product} added to cart (shipping cost us ${shippingCost})`
-//         );
-//     };
-
-//     const orderStock = function (product, quantity) {
-//         console.log(`${quantity} ${product} ordered from supplier`);
-//     };
-
-//     return {
-//         addToCart,
-//         cart,
-//         totalPrice,
-//         totalQuantity,
-//     };
-// })();
-
-// ShoppingCart2.addToCart("apple", 4);
-// ShoppingCart2.addToCart("pizza", 2);
-// console.log(ShoppingCart2);
-// console.log(ShoppingCart2.shippingCost);
-
-// CommonJS Modules
-// Export
-// export.addToCart = function (product, quantity) {
-//     cart.push({ product, quantity });
-//     console.log(
-//         `${quantity} ${product} added to cart (shipping cost us ${shippingCost})`
-//     );
-// };
-
-// Import
-// const { addToCart } = require("./shoppingCart.js");
+console.log(stateDeepClone);
