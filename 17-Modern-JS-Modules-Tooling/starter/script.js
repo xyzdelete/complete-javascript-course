1769fd1931109dadea12f57c71c3f1f0bcef2567
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-17 14:46:13 +0200

    273. Top-Level await (ES2022)

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,20 +1,22 @@
-// Importing module
-//import { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
-// addToCart("bread", 5);
-// console.log(price, tq);
-console.log("Importing module");
-// console.log(shippingCost);
+// console.log("Start fetching");
+// const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+// const data = await res.json();
+// console.log(data);
+// console.log("Something");
 
 // import * as ShoppingCart from "./shoppingCart.js";
-// ShoppingCart.addToCart("bread", 5);
-// console.log(ShoppingCart.totalPrice);
 
-// import add, { addToCart, totalPrice as price, tq } from "./shoppingCart.js";
-// console.log(price)
+const getLastPost = async function () {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+    const data = await res.json();
+    console.log(data);
+    return { title: data.at(-1).title, text: data.at(-1).body };
+};
 
-import add, { cart } from "./shoppingCart.js";
-add("pizza", 2);
-add("bread", 5);
-add("apples", 4);
+const lastPost = getLastPost();
+console.log(lastPost);
 
-console.log(cart);
+// Not very clean
+// lastPost.then((last) => console.log(last));
+const lastPost2 = await getLastPost();
+console.log(lastPost2);
