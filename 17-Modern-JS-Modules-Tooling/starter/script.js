e5f85f5e523301512a6469ab6f1983c1edf1d9a6
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-17 15:04:46 +0200

    275. CommonJS Modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -21,32 +21,44 @@
 // const lastPost2 = await getLastPost();
 // console.log(lastPost2);
 
-const ShoppingCart2 = (function () {
-    const cart = [];
-    const shippingCost = 10;
-    const totalPrice = 237;
-    const totalQuantity = 23;
+// const ShoppingCart2 = (function () {
+//     const cart = [];
+//     const shippingCost = 10;
+//     const totalPrice = 237;
+//     const totalQuantity = 23;
 
-    const addToCart = function (product, quantity) {
-        cart.push({ product, quantity });
-        console.log(
-            `${quantity} ${product} added to cart (shipping cost us ${shippingCost})`
-        );
-    };
+//     const addToCart = function (product, quantity) {
+//         cart.push({ product, quantity });
+//         console.log(
+//             `${quantity} ${product} added to cart (shipping cost us ${shippingCost})`
+//         );
+//     };
 
-    const orderStock = function (product, quantity) {
-        console.log(`${quantity} ${product} ordered from supplier`);
-    };
+//     const orderStock = function (product, quantity) {
+//         console.log(`${quantity} ${product} ordered from supplier`);
+//     };
 
-    return {
-        addToCart,
-        cart,
-        totalPrice,
-        totalQuantity,
-    };
-})();
+//     return {
+//         addToCart,
+//         cart,
+//         totalPrice,
+//         totalQuantity,
+//     };
+// })();
 
-ShoppingCart2.addToCart("apple", 4);
-ShoppingCart2.addToCart("pizza", 2);
-console.log(ShoppingCart2);
-console.log(ShoppingCart2.shippingCost);
+// ShoppingCart2.addToCart("apple", 4);
+// ShoppingCart2.addToCart("pizza", 2);
+// console.log(ShoppingCart2);
+// console.log(ShoppingCart2.shippingCost);
+
+// CommonJS Modules
+// Export
+// export.addToCart = function (product, quantity) {
+//     cart.push({ product, quantity });
+//     console.log(
+//         `${quantity} ${product} added to cart (shipping cost us ${shippingCost})`
+//     );
+// };
+
+// Import
+// const { addToCart } = require("./shoppingCart.js");
