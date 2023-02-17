0f988edd1a71fc69e0d0598c2a688b2f45950f15
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-17 20:00:15 +0200

    278. Bundling With Parcel and NPM Scripts

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -1,4 +1,11 @@
-import cloneDeep from "./node_modules/lodash-es/cloneDeep.js";
+import cloneDeep from "lodash-es";
+import add, { cart } from "./shoppingCart.js";
+
+add("pizza", 2);
+add("bread", 5);
+add("apples", 4);
+
+console.log(cart);
 
 const state = {
     cart: [
@@ -11,7 +18,11 @@ const state = {
 const stateClone = Object.assign({}, state);
 const stateDeepClone = cloneDeep(state);
 
-console.log(stateClone);
 state.user.loggedIn = false;
+console.log(stateClone);
 
 console.log(stateDeepClone);
+
+if (module.hot) {
+    module.hot.accept();
+}
