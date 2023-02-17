3c535052d05cf744de3401f9f676b451205016e2
Arturs Anikins <xyzdelete@protonmail.com>
2023-02-17 14:58:50 +0200

    274. The Module Pattern

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -6,17 +6,47 @@
 
 // import * as ShoppingCart from "./shoppingCart.js";
 
-const getLastPost = async function () {
-    const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
-    const data = await res.json();
-    console.log(data);
-    return { title: data.at(-1).title, text: data.at(-1).body };
-};
+// const getLastPost = async function () {
+//     const res = await fetch(`https://jsonplaceholder.typicode.com/posts`);
+//     const data = await res.json();
+//     console.log(data);
+//     return { title: data.at(-1).title, text: data.at(-1).body };
+// };
 
-const lastPost = getLastPost();
-console.log(lastPost);
+// const lastPost = getLastPost();
+// console.log(lastPost);
 
-// Not very clean
-// lastPost.then((last) => console.log(last));
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// // Not very clean
+// // lastPost.then((last) => console.log(last));
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+const ShoppingCart2 = (function () {
+    const cart = [];
+    const shippingCost = 10;
+    const totalPrice = 237;
+    const totalQuantity = 23;
+
+    const addToCart = function (product, quantity) {
+        cart.push({ product, quantity });
+        console.log(
+            `${quantity} ${product} added to cart (shipping cost us ${shippingCost})`
+        );
+    };
+
+    const orderStock = function (product, quantity) {
+        console.log(`${quantity} ${product} ordered from supplier`);
+    };
+
+    return {
+        addToCart,
+        cart,
+        totalPrice,
+        totalQuantity,
+    };
+})();
+
+ShoppingCart2.addToCart("apple", 4);
+ShoppingCart2.addToCart("pizza", 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost);
